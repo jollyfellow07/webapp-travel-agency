d17680ad8be2164bf5e3ac2caf755b16f5e23800
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-23 16:35:06 +0200

    Ora visualizzo i miei oggetti in json

---
+++ Viaggiatore/Controllers/API/BoxController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Viaggiatore.Data;
+using Viaggiatore.Models;
+
+namespace Viaggiatore.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BoxController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            List<Pacchetto> boxViaggi;
+            using (ViaggioContext db = new ViaggioContext())
+            {
+                boxViaggi = db.pacchetti.ToList();
+            }
+            return Ok(boxViaggi);
+        }
+    }
+}
---
+++ Viaggiatore/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Viaggiatore.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
